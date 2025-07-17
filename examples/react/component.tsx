03281945de3d0b9c2a8281eb8ac571588865bc2c
Jan Kumer <jan@planet.com>
2025-07-17 15:29:02 +0200

    Add ts,tsx file extensions support to planet/react

---
+++ examples/react/component.tsx
@@ -0,0 +1,23 @@
+import {string} from 'prop-types';
+import React, {useCallback, useState} from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloMessage = ({name}) => {
+  const [greeting, setGreeting] = useState('Hello');
+
+  const onClick = useCallback(() => {
+    setGreeting(`Goodbye ${name}`);
+  }, [name]);
+
+  return (
+    <div onClick={onClick}>
+      {greeting} {name}
+    </div>
+  );
+};
+
+HelloMessage.propTypes = {
+  name: string.isRequired,
+};
+
+ReactDOM.render(<HelloMessage name="John" />, document.getElementById('root'));
